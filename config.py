9cb3c564b85013ec892d46a00678d449c411e1b6
Michael Wheeler <wheeler.m@northeastern.edu>
2020-12-10 10:25:30 -0500

    Set word vectors to train on one epoch only

--- config.py
+++ config.py
@@ -18,9 +18,9 @@ LINE_SENTENCE_CORPUS_FILE = DATA_DIR / "line_sentence_corpus.txt"
 
 WORD_VECTOR_SIZE = 200
 FASTTEXT_INIT = {"size": WORD_VECTOR_SIZE, "window": 4, "min_count": 1, "corpus_file": str(LINE_SENTENCE_CORPUS_FILE),
-                 "workers": 8}
+                 "workers": 8, "iter": 1}
 WORD2VEC_INIT = {"size": WORD_VECTOR_SIZE, "window": 4, "min_count": 1, "corpus_file": str(LINE_SENTENCE_CORPUS_FILE),
-                 "workers": 8}
+                 "workers": 8, "iter": 1}
 WORD_VECTOR_DIR = DATA_DIR / "word_vectors"
 FASTTEXT_FILE = WORD_VECTOR_DIR / "fasttext.model"
 WORD2VEC_FILE = WORD_VECTOR_DIR / "word2vec.model"
--- pipeline/__main__.py
+++ pipeline/__main__.py
@@ -1,13 +1,10 @@
 from luigi import build
 
-from pipeline.extract_from_xml_tags import CorpusXMLToParquet
 from pipeline.train_word_embeddings import TrainWordEmbeddings
 from pipeline.embed_article_text import GenerateDocumentEmbeddings
 from pipeline.perform_clustering import PerformSpectralClustering
 
 if __name__ == "__main__":
-    tasks = [CorpusXMLToParquet()]
-    build(tasks, local_scheduler=True)
     
     tasks = [TrainWordEmbeddings()]
     build(tasks, local_scheduler=True)
