a09b5e1422628e1dc9c11750e9c655a5a8083eb9
Michael Wheeler <wheeler.m@northeastern.edu>
2020-12-10 22:58:48 -0500

    Finished implementation of spectral clustering task

--- pipeline/perform_clustering.py
+++ pipeline/perform_clustering.py
@@ -1,9 +1,9 @@
-"""Assembles a Dask Dataframe from the Parquet files creaated by upstream tasks and performs Spectral Clustering
+"""Assembles a Dask Dataframe from the Parquet files created by upstream tasks and performs Spectral Clustering
    experiments on the data."""
 
 import dask.dataframe
 from dask_ml.cluster import SpectralClustering
-from luigi import Task, LocalTarget, IntParameter
+from luigi import Task, LocalTarget, IntParameter, Parameter
 
 import config
 from pipeline.embed_article_text import GenerateDocumentEmbeddings
@@ -14,23 +14,30 @@ class PerformSpectralClustering(Task):
        the magic of Dask-ML."""
     
     num_clusters = IntParameter()
+    word_vectors = Parameter()
     
     def requires(self):
-        return [GenerateDocumentEmbeddings()]
+        return [GenerateDocumentEmbeddings(model=self.word_vectors)]
 
     def output(self):
-        return LocalTarget(config.CLUSTERING_RESULTS_DIR / "output.txt")  # FIXME actual target
+        return LocalTarget(config.CLUSTERING_RESULTS_DIR / f'cluster_{self.num_clusters}_{self.word_vectors}.txt')
     
     def run(self):
-        ddf = dask.dataframe.read_parquet(None)
+        if self.word_vectors not in {"fasttext", "word2vec"}:
+            raise ValueError(f'Expected fasttext or word2vec; got {self.word_vectors}')
         
-        # TODO set ID column as index, drop features except the document vectors
+        ddf = dask.dataframe.read_parquet(config.ARTICLE_EMBEDDINGS_DIR / self.word_vectors)
         
-        # Perform spectral clustering
-        clustering_model = SpectralClustering(n_clusters=self.num_clusters)
-        clustered_ddf = clustering_model.fit(ddf)
+        # Perform spectral clustering on the vectorized word columns only
+        clustering_model = SpectralClustering(n_clusters=self.num_clusters, n_jobs=-1, persist_embedding=True)
+        cluster_labels = clustering_model.fit(ddf.drop(["id", "url", "title", "tokenized_text"], axis=1))
         
-        # TODO reattach other columns to the ddf, write to disk, return
+        # Attach cluster labels to ddf and write the augmented ddf to disk
+        ddf = ddf.join(cluster_labels)
+        ddf_output_path = config.CLUSTERING_RESULTS_DIR / self.word_vectors
+        ddf_output_path.mkdir(parents=True, exist_ok=True)
+        dask.dataframe.to_parquet(ddf, ddf_output_path)
         
+        # And save the parameters of the spectral clustering object to the target
         with self.output().open("w") as f:
-            f.write("Success!")
+            f.write(clustering_model.get_params(deep=True))
