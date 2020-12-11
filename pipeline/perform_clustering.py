671db97b4e999515f5a9b6d7165ee990e21e3140
Michael Wheeler <wheeler.m@northeastern.edu>
2020-12-11 12:06:23 -0500

    Lil bit of code for evaluation via silhouette score

--- pipeline/perform_clustering.py
+++ pipeline/perform_clustering.py
@@ -4,6 +4,7 @@
 import dask.dataframe
 from dask_ml.cluster import SpectralClustering
 from luigi import Task, LocalTarget, IntParameter, Parameter
+from sklearn.metrics import silhouette_score
 
 import config
 from pipeline.embed_article_text import GenerateDocumentEmbeddings
@@ -27,10 +28,11 @@ class PerformSpectralClustering(Task):
             raise ValueError(f'Expected fasttext or word2vec; got {self.word_vectors}')
         
         ddf = dask.dataframe.read_parquet(config.ARTICLE_EMBEDDINGS_DIR / self.word_vectors)
+        X = ddf.drop(["id", "url", "title", "tokenized_text"], axis=1)
         
         # Perform spectral clustering on the vectorized word columns only
         clustering_model = SpectralClustering(n_clusters=self.num_clusters, n_jobs=-1, persist_embedding=True)
-        cluster_labels = clustering_model.fit(ddf.drop(["id", "url", "title", "tokenized_text"], axis=1))
+        cluster_labels = clustering_model.fit(X)
         
         # Attach cluster labels to ddf and write the augmented ddf to disk
         ddf = ddf.join(cluster_labels)
@@ -38,6 +40,13 @@ class PerformSpectralClustering(Task):
         ddf_output_path.mkdir(parents=True, exist_ok=True)
         dask.dataframe.to_parquet(ddf, ddf_output_path)
         
+        # Evaluate using silhouette score metric
+        # TODO: move this to a different task so the clustering can succeed independent of the evaluation
+        # TODO: will sklearn's function work on dask objects?
+        silhouette_score_result = silhouette_score(X, cluster_labels)
+        print(f'Clustering {self.word_vectors} k={self.num_clusters}: {silhouette_score_result}')
+        
         # And save the parameters of the spectral clustering object to the target
         with self.output().open("w") as f:
+            f.write(f'Clustering {self.word_vectors} k={self.num_clusters}: {silhouette_score_result}' + "\n")
             f.write(clustering_model.get_params(deep=True))
