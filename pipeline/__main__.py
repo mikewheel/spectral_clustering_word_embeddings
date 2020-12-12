664d5a6c49d20b36ca0c01aaf3ca0e7ef13cebfa
Michael Wheeler <wheeler.m@northeastern.edu>
2020-12-12 14:40:38 -0500

    First run of clustering tasks

--- pipeline/__main__.py
+++ pipeline/__main__.py
@@ -5,5 +5,6 @@ from pipeline.perform_clustering import PerformSpectralClustering
 
 if __name__ == "__main__":
     
-    tasks = [GenerateAllDocumentEmbeddings()]
+    tasks = [GenerateAllDocumentEmbeddings(), PerformSpectralClustering(num_clusters=20, word_vectors="fasttext"),
+             PerformSpectralClustering(num_clusters=20, word_vectors="word2vec")]
     build(tasks, local_scheduler=True)
