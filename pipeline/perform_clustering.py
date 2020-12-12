9f9077485a74c3178d1febaf53ffdadeb1779e51
Michael Wheeler <wheeler.m@northeastern.edu>
2020-12-12 14:42:23 -0500

    Convert design matrix to Dask array

--- pipeline/perform_clustering.py
+++ pipeline/perform_clustering.py
@@ -28,7 +28,7 @@ class PerformSpectralClustering(Task):
             raise ValueError(f'Expected fasttext or word2vec; got {self.word_vectors}')
         
         ddf = dask.dataframe.read_parquet(config.ARTICLE_EMBEDDINGS_DIR / self.word_vectors)
-        X = ddf.drop(["id", "url", "title", "tokenized_text"], axis=1)
+        X = ddf.drop(["id", "url", "title", "tokenized_text"], axis=1).to_dask_array(lengths=True)
         
         # Perform spectral clustering on the vectorized word columns only
         clustering_model = SpectralClustering(n_clusters=self.num_clusters, n_jobs=-1, persist_embedding=True)
